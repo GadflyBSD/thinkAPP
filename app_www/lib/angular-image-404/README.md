fa58415d5b0bd4402fc29dc27fa0f13d1f9d5fd4
刘海明 <GadflyBSD@gmail.com>
2017-05-17 11:20:55 +0800

    no message

--- app_www/lib/angular-image-404/README.md
+++
@@ -1,1 +0,0 @@
-[DEMO of angular-image-404](http://stiekel.github.io/angular-image-404/)
--- app_www/lib/angular-image-404/example/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', ['angular-image-404']);
-
-app.controller('TestController', TestController);
-
-function TestController() {
-
-  this.image = 'http://placeholder.it'
-}
